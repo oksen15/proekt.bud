95a63331858f094592196d887abd988034026959
oksen15 <124519752+oksen15@users.noreply.github.com>
2023-03-11 23:29:00 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,16 +7,8 @@ function showForm() {
 
 
 
-
-  function sendMessage(event) {
-    event.preventDefault();
-  
-    var name = document.getElementById("name").value;
-    var phone = document.getElementById("phone").value;
-  
-    var url = `https://api.telegram.org/bot6196812518:AAHCIQdfWE1Pu-Ei2B5avacL_pg5OcA5OM0/sendMessage?chat_id=191899857&text=${name}_${phone}`;
-    
-    return url;
-
-    
-  }
+function sendToLink() {
+  var nameValue = document.getElementById("name").value;
+  var link = "https://api.telegram.org/bot6196812518:AAHCIQdfWE1Pu-Ei2B5avacL_pg5OcA5OM0/sendMessage?chat_id=191899857&text=" + nameValue;
+  window.location.href = link;
+}
