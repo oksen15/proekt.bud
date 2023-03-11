5921f9cdaaff0784b6bb29dcfadd79ccf2087dbc
oksen15 <124519752+oksen15@users.noreply.github.com>
2023-03-11 23:43:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function showForm() {
 
 function sendToLink() {
   var nameValue = document.getElementById("name").value;
-  var link = "https://api.telegram.org/bot6196812518:AAHCIQdfWE1Pu-Ei2B5avacL_pg5OcA5OM0/sendMessage?chat_id=191899857&text=" + nameValue;
+  var phoneValue = document.getElementById("phone").value;
+  var link = "https://api.telegram.org/bot6196812518:AAHCIQdfWE1Pu-Ei2B5avacL_pg5OcA5OM0/sendMessage?chat_id=191899857&text=name=" + nameValue + "&phone=" + phoneValue;
   window.location.href = link;
 }
