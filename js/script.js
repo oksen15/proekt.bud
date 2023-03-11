b3f7b93a376101c6ea72bd9a9acd2c252dc13eef
oksen15 <124519752+oksen15@users.noreply.github.com>
2023-03-11 23:15:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
- 
 function showForm() {
     var form = document.getElementById("myForm");
     form.style.display = "block";
@@ -9,23 +8,15 @@ function showForm() {
 
 
 
-
-function sendMessage(event) {
+  function sendMessage(event) {
     event.preventDefault();
   
     var name = document.getElementById("name").value;
     var phone = document.getElementById("phone").value;
   
-    var url = "https://api.telegram.org/6196812518:AAHCIQdfWE1Pu-Ei2B5avacL_pg5OcA5OM0/sendMessage";
-    var data = {
-      chat_id: "-191899857",
-      text: "Имя: " + name + "\nНомер телефона: " + phone
-    };
-  
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(JSON.stringify(data));
-  
-    return false;
+    var url = `https://api.telegram.org/bot6196812518:AAHCIQdfWE1Pu-Ei2B5avacL_pg5OcA5OM0/sendMessage?chat_id=191899857&text=${name}_${phone}`;
+    
+    location.href=url;
+
+    
   }
