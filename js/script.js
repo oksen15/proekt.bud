001b8410d3f0ad5906b282e2d3ca7b1df9a8153d
oksen15 <124519752+oksen15@users.noreply.github.com>
2023-03-11 23:58:18 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,20 @@ function showForm() {
 
 
 
+function sendToTelegram() {
+  var nameValue = document.getElementById("name").value;
+  var phoneValue = document.getElementById("phone").value;
+  var telegramURL = "https://api.telegram.org/bot<ваш_токен>/sendMessage?chat_id=<ID_чата>&text=" + encodeURIComponent("Имя: " + nameValue + ", Номер телефона: " + phoneValue);
+  window.location.href = telegramURL;
+}
+</script>
+
+
+
+
+
+
+
 // function sendToLink() {
 //   var nameValue = document.getElementById("name").value;
 //   var phoneValue = document.getElementById("phone").value;
@@ -16,11 +30,11 @@ function showForm() {
 
 
 
-function sendToLink() {
-  var nameValue = document.getElementById("name").value;
-  var phoneValue = document.getElementById("phone").value;
-  var url = "https://api.telegram.org/bot6196812518:AAHCIQdfWE1Pu-Ei2B5avacL_pg5OcA5OM0/sendMessage?chat_id=191899857&text=Имя: " + nameValue + ", Номер телефона: " + phoneValue;
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.send();
-}
+// function sendToLink() {
+//   var nameValue = document.getElementById("name").value;
+//   var phoneValue = document.getElementById("phone").value;
+//   var url = "https://api.telegram.org/bot6196812518:AAHCIQdfWE1Pu-Ei2B5avacL_pg5OcA5OM0/sendMessage?chat_id=191899857&text=Имя: " + nameValue + ", Номер телефона: " + phoneValue;
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("GET", url);
+//   xhr.send();
+// }
